2980b447c5fba4f2c50f7d11740f40c59e47a428
Sanjeev Kumar <105745275+Sanju-Barsiwal@users.noreply.github.com>
2025-11-10 16:17:29 +0530

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { ChevronDown } from "lucide-react";
-
+// import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { cn } from "@/components/lib/utils";
 
 const Accordion = AccordionPrimitive.Root;
